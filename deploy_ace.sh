1d042554ad3e1d854a2e5525575870c99e64c5cf
robomeister <robomeister@users.noreply.github.com>
2020-08-21 07:28:27 -0400

    Update deploy_ace.sh

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -10,8 +10,8 @@ cat deploy-ace.json |  jq '.metadata.name = "'${IDS_PROJECT_NAME}'-'${NAMESPACE}
 echo "DEPLOYING..."
 cat deploy-ace.json |  jq '.metadata.name = "'${IDS_PROJECT_NAME}'-'${NAMESPACE}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
 
-echo "Sleeping for five seconds"
-sleep 5
+echo "Sleeping for several seconds"
+sleep 9
 
 echo "deleting routes"
 echo "oc -n ${NAMESPACE}  delete route ${IDS_PROJECT_NAME}-${NAMESPACE}-http"
