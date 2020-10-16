77e7705e394a9e3e188ebab6a9e369eeb7b7e7a4
robomeister <robomeister@users.noreply.github.com>
2020-10-16 06:57:10 -0400

    Update deploy_ace.sh

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -3,46 +3,75 @@
 env
 
 wget https://raw.githubusercontent.com/robomeister/crds/master/deploy-ace.json
+wget https://raw.githubusercontent.com/robomeister/crds/master/deploy-ace-prod.json
 
-cat deploy-ace.json
+case $NAMESPACE in
+
+  *"dev"*)
+    echo -n "Using non-prod deployment json"
+    cp deploy-ace.json deploy.json
+    ;;
+
+  *"sit"*)
+    echo -n "Using non-prod deployment json"
+    cp deploy-ace.json deploy.json
+    ;;
+
+  *"uat"*)
+    echo -n "Using non-prod deployment json"
+    cp deploy-ace.json deploy.json
+    ;;
+
+  *"pt"*)
+    echo -n "Using prod deployment json"
+    cp deploy-ace-prod.json deploy.json
+    ;;
+
+  *"prod"*)
+    echo -n "Using prod deployment json"
+    cp deploy-ace-prod.json deploy.json
+    ;;
+esac
+
+cat deploy.json
 
 if [[ -z ${SERVER_CONF} ]];
 then
-      cp  deploy-ace.json deploy-conf.json
+      cp  deploy.json deploy1.json
 else
-      cat deploy-ace.json | jq '.spec.configurations += ["'${SERVER_CONF}'"]' > deploy-conf.json
+      cat deploy.json | jq '.spec.configurations += ["'${SERVER_CONF}'"]' > deploy1.json
 fi
 
 if [[ -z ${MAX_CPU} ]];
 then
-      cp  deploy-conf.json deploy-max-cpu.json
+      cp  deploy1.json deploy2.json
 else
-      cat deploy-conf.json | jq '.spec.pod.containers.runtime.resources.limits.cpu="'${MAX_CPU}'"' > deploy-max-cpu.json
+      cat deploy1.json | jq '.spec.pod.containers.runtime.resources.limits.cpu="'${MAX_CPU}'"' > deploy2.json
 fi
 
 if [[ -z ${MAX_MEMORY} ]];
 then
-      cp  deploy-max-cpu.json deploy-max-mem.json
+      cp  deploy2.json deploy3.json
 else
-      cat deploy-max-cpu.json | jq '.spec.pod.containers.runtime.resources.limits.memory="'${MAX_MEMORY}'"' > deploy-max-mem.json
+      cat deploy2.json | jq '.spec.pod.containers.runtime.resources.limits.memory="'${MAX_MEMORY}'"' > deploy3.json
 fi
 
 if [[ -z ${MIN_CPU} ]];
 then
-      cp  deploy-max-mem.json deploy-min-cpu.json
+      cp  deploy3.json deploy4.json
 else
-      cat deploy-max-mem.json | jq '.spec.pod.containers.runtime.resources.requests.cpu="'${MIN_CPU}'"' > deploy-min-cpu.json
+      cat deploy3.json | jq '.spec.pod.containers.runtime.resources.requests.cpu="'${MIN_CPU}'"' > deploy4.json
 fi
 
 if [[ -z ${MIN_MEMORY} ]];
 then
-      cp  deploy-min-cpu.json deploy-min-mem.json
+      cp  deploy4.json deploy5.json
 else
-      cat deploy-min-cpu.json | jq '.spec.pod.containers.runtime.resources.requests.memory="'${MIN_MEMORY}'"' > deploy-min-mem.json
+      cat deploy4.json | jq '.spec.pod.containers.runtime.resources.requests.memory="'${MIN_MEMORY}'"' > deploy5.json
 fi
 
 echo "DRY RUN..."
-cat deploy-min-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
+cat deploy5.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
 
 echo "DEPLOYING..."
-cat deploy-min-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
+cat deploy5.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
