28874745c8339ed7d1ea4c60da0d6a308cb8b8eb
robomeister <robomeister@users.noreply.github.com>
2020-10-15 21:45:08 -0400

    Update deploy_ace.sh

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -8,19 +8,39 @@ cat deploy-ace.json
 
 
 
-if [[ -z ${SERVERCONF} ]];
+if [[ -z ${SERVER_CONF} ]];
 then
       cp deploy-ace.json deploy-conf.json
 else
-      echo "Adding server configuration ${SERVERCONF}"
-      cat deploy-ace.json | jq '.spec.configurations += ["'${SERVERCONF}'"]' > deploy-conf.json
+      echo "Adding server configuration ${SERVER_CONF}"
+      cat deploy-ace.json | jq '.spec.configurations += ["'${SERVER_CONF}'"]' > deploy-conf.json
 fi
 
 echo "SERVERCONF"
 cat deploy-conf.json
 
+if [[ -z ${MAX_CPU} ]];
+then
+      cp deploy-conf.json deploy-cpu.json
+else
+      cat deploy-conf.json | jq '.spec.pod.containers.runtime.resources.limits.cpu="${MAX_CPU}"' > deploy-cpu.json
+fi
+
+echo "CPU"
+cat deploy-cpu.json
+
+if [[ -z ${MAX_MEMORY} ]];
+then
+      cp deploy-cpu.json deploy-mem.json
+else
+      cat deploy-cpu.json | jq '.spec.pod.containers.runtime.resources.limits.memory="${MAX_MEMORY}"' > deploy-mem.json
+fi
+
+echo "MEMORY"
+cat deploy-mem.json
+
 echo "DRY RUN..."
-cat deploy-conf.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
+cat deploy-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
 
 echo "DEPLOYING..."
-cat deploy-conf.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
+cat deploy-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
