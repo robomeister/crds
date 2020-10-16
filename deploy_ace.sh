309f3075126f3a9fed11ef9a1c776102af4a7c5a
robomeister <robomeister@users.noreply.github.com>
2020-10-15 21:17:54 -0400

    Update deploy_ace.sh

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -12,10 +12,11 @@ if [[ -z ${SERVERCONF} ]];
 then
       echo "\$SERVERCONF is not set"
 else
-      cat deploy-ace.json | jq '.spec.configurations += ["'${SERVERCONF}'"]' > deploy-ace.json
+      echo "TEST"
+      cat deploy-ace.json | jq '.spec.configurations += ["'${SERVERCONF}'"]' 
+      echo "DONE TEST"
 fi
 
-cat deploy-ace.json
 
 echo "DRY RUN..."
 cat deploy-ace.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
