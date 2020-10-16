0dd12e3cc95280a1e117b0bc925bdea4d4ce5033
robomeister <robomeister@users.noreply.github.com>
2020-10-15 22:02:02 -0400

    Update deploy_ace.sh

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -6,41 +6,43 @@ wget https://raw.githubusercontent.com/robomeister/crds/master/deploy-ace.json
 
 cat deploy-ace.json
 
-
-
 if [[ -z ${SERVER_CONF} ]];
 then
-      cp deploy-ace.json deploy-conf.json
+      cp  deploy-ace.json deploy-conf.json
 else
-      echo "Adding server configuration ${SERVER_CONF}"
       cat deploy-ace.json | jq '.spec.configurations += ["'${SERVER_CONF}'"]' > deploy-conf.json
 fi
 
-echo "SERVERCONF"
-cat deploy-conf.json
-
 if [[ -z ${MAX_CPU} ]];
 then
-      cp deploy-conf.json deploy-cpu.json
+      cp  deploy-conf.json deploy-max-cpu.json
 else
-      cat deploy-conf.json | jq '.spec.pod.containers.runtime.resources.limits.cpu="'${MAX_CPU}'"' > deploy-cpu.json
+      cat deploy-conf.json | jq '.spec.pod.containers.runtime.resources.limits.cpu="'${MAX_CPU}'"' > deploy-max-cpu.json
 fi
-
-echo "CPU"
-cat deploy-cpu.json
 
 if [[ -z ${MAX_MEMORY} ]];
 then
-      cp deploy-cpu.json deploy-mem.json
+      cp  deploy-max-cpu.json deploy-max-mem.json
 else
-      cat deploy-cpu.json | jq '.spec.pod.containers.runtime.resources.limits.memory="'${MAX_MEMORY}'"' > deploy-mem.json
+      cat deploy-max-cpu.json | jq '.spec.pod.containers.runtime.resources.limits.memory="'${MAX_MEMORY}'"' > deploy-max-mem.json
 fi
 
-echo "MEMORY"
-cat deploy-mem.json
+if [[ -z ${MIN_CPU} ]];
+then
+      cp  deploy-max-mem.json deploy-min-cpu.json
+else
+      cat deploy-max-mem.json | jq '.spec.pod.containers.runtime.resources.limits.cpu="'${MIN_CPU}'"' > deploy-min-cpu.json
+fi
+
+if [[ -z ${MIN_MEMORY} ]];
+then
+      cp  deploy-min-cpu.json deploy-min-mem.json
+else
+      cat deploy-min-cpu.json | jq '.spec.pod.containers.runtime.resources.limits.memory="'${MIN_MEMORY}'"' > deploy-min-mem.json
+fi
 
 echo "DRY RUN..."
-cat deploy-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
+cat deploy-min-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
 
 echo "DEPLOYING..."
-cat deploy-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
+cat deploy-min-mem.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
