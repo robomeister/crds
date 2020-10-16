f8d693114b4b6643c5e3daab2e82a4b82f2d198d
robomeister <robomeister@users.noreply.github.com>
2020-10-15 21:33:55 -0400

    Update deploy_ace.sh

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -12,22 +12,15 @@ if [[ -z ${SERVERCONF} ]];
 then
       cp deploy-ace.json deploy-conf.json
 else
+      echo "Adding server configuration ${SERVERCONF}"
       cat deploy-ace.json | jq '.spec.configurations += ["'${SERVERCONF}'"]' > deploy-conf.json
 fi
 
+echo "SERVERCONF"
+cat deploy-conf.json
 
 echo "DRY RUN..."
 cat deploy-conf.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - --dry-run -o yaml
 
 echo "DEPLOYING..."
 cat deploy-conf.json |  jq '.metadata.name = "'${NAMESPACE}'-'${IDS_PROJECT_NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.pod.containers.runtime.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.replicas='${REPLICAS}'' | oc apply -f - 
-
-#echo "Sleeping for several seconds"
-#sleep 15
-
-#echo "deleting routes"
-#echo "oc -n ${NAMESPACE}  delete route ${IDS_PROJECT_NAME}-${NAMESPACE}-http"
-#oc -n ${NAMESPACE}  delete route ${IDS_PROJECT_NAME}-${NAMESPACE}-http
-
-#echo "-n ${NAMESPACE}  delete route ${IDS_PROJECT_NAME}-${NAMESPACE}-https"
-#oc -n ${NAMESPACE}  delete route ${IDS_PROJECT_NAME}-${NAMESPACE}-https
