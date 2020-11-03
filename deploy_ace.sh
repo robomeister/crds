5d338485b7f9351ea165c0a31e6b9f3cc3a0c638
robobob <robobob@ca.ibm.com>
2020-11-03 06:56:20 -0500

    replace instead of apply

--- deploy_ace.sh
+++ deploy_ace.sh
@@ -94,6 +94,7 @@ if [[ -z ${MATCH_SELECTOR} ]];
 then
    echo "No Match Selector Specified"
 else
-   oc -n ${NAMESPACE} get deployment ${DEPLOYMENT_NAME} -o json | jq '.spec.selector.matchLabels.'${MATCH_SELECTOR}'="true" | .metadata.labels.'${MATCH_SELECTOR}'="true" | .spec.template.metadata.labels.'${MATCH_SELECTOR}'="true"' | oc -n ${NAMESPACE} apply --force -f -
+   echo "Re-applying the deployment..."
+   oc -n ${NAMESPACE} get deployment ${DEPLOYMENT_NAME} -o json | jq '.spec.selector.matchLabels.'${MATCH_SELECTOR}'="true" | .metadata.labels.'${MATCH_SELECTOR}'="true" | .spec.template.metadata.labels.'${MATCH_SELECTOR}'="true"' | oc -n ${NAMESPACE} replace --force -f -
 fi
 
