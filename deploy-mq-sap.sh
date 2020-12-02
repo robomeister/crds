226b6e927264d5867d69df6740db334b4668bcd8
Marc Verhiel <mverhiel@ca.ibm.com>
2020-12-02 09:42:10 -0800

    added affinity rules

--- deploy-mq-sap.sh
+++ deploy-mq-sap.sh
@@ -48,15 +48,37 @@ else
    cat deploy-mq-sap-2.json | jq '.metadata.name = "'${NAMESPACE}'-'${NAME}'"' | jq '.metadata.namespace = "'${NAMESPACE}'"' | jq '.spec.queueManager.image="'${PIPELINE_IMAGE_URL}'"' |  jq '.spec.queueManager.mqsc[0].configMap.name="'${NAME}'" | .spec.queueManager.mqsc[0].configMap.items[0]="20-'${NAME}'.mqsc"' >deploy-mq-sap-3.json
 fi
 
+if [[ -z ${PRIMARY_NODE} ]];
+then
+   
+   echo "Not assigning qmgr to specific worker node"
+   cat deploy-mq-1.json > deploy-mq-4.json
+   
+else
+	echo "Primary node assignd, checking for secondary"
+
+	if [[ -z ${SECONDARY_NODE} ]];
+	then
+	   echo "Please set both PRIMARY_NODE and SECONDARY_NODE environment variables"
+	   exit 1
+	else
+	   echo "Setting spec.affinity.nodeAffinity"
+	   cat deploy-mq-sap-3.json | jq '.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions += [{"key":"workernode","operator":"In", "values":["'${PRIMARY_NODE}'","'${SECONDARY_NODE}'"]}]' > deploy-mq-sap-4.json
+	   cat deploy-mq-sap-4.json | jq '.spec.affinity.nodeAffinity.preferredDuringSchedulingIgnoredDuringExecution[0].preference.matchExpressions += [{"key":"workernode","operator":"In", "values":["'${PRIMARY_NODE}'"]}]' > deploy-mq-sap-5.json
+	   cat deploy-mq-sap-5.json | jq '.spec.affinity.nodeAffinity.preferredDuringSchedulingIgnoredDuringExecution[0].weight= 1' > deploy-mq-sap-6.json
+	fi   
+fi
+
+
 echo "final json created "
 echo "*** start of json to deploy  ***"
-cat deploy-mq-sap-3.json
+cat deploy-mq-sap-6.json
 echo "*** end of json to deploy ***"
 echo "deploying - dry run"
-cat deploy-mq-sap-3.json | oc apply -f - --dry-run -o yaml 
+cat deploy-mq-sap-6.json | oc apply -f - --dry-run -o yaml 
 
 echo "deploying"
-cat deploy-mq-sap-3.json | oc apply -f -  
+cat deploy-mq-sap-6.json | oc apply -f -  
 
 echo "wait a few seconds for service to create"
 sleep 9
