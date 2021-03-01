6362e4eab3e55d710c967b61d2346031b324ea84
Marc Verhiel <mverhiel@ca.ibm.com>
2021-03-01 11:55:25 -0800

    added pvc support to pco

--- deploy_ace_pco.sh
+++ deploy_ace_pco.sh
@@ -130,7 +130,7 @@ sleep 10s
 DEPLOYMENT_NAME=${NAMESPACE}-${IDS_PROJECT_NAME}-is
 
 set -x
-if oc rollout status deploy/${DEPLOYMENT_NAME} --watch=true --request-timeout="1800s" --namespace ${NAMESPACE}; then
+if oc rollout status deploy/${DEPLOYMENT_NAME} --watch=true --request-timeout="1200s" --namespace ${NAMESPACE}; then
   STATUS="pass"
 else
   STATUS="fail"
@@ -141,22 +141,42 @@ if [ "$STATUS" == "fail" ]; then
   echo "DEPLOYMENT FAILED"
   exit 1
 else
+   oc -n ${NAMESPACE} get deployment ${DEPLOYMENT_NAME} -o json >deployed.json
+
    if [[ -z ${MATCH_SELECTOR} ]];
    then
-      echo "No Match Selector Specified"
+      echo "No Match Selector Specified.  Enabling metrics and setting log4j PVC..."
+	  cat deployed.json | jq '.spec.template.spec.containers[0].volumeMounts += [{"mountPath": "/home/aceuser/ace-server/log4j/logs", "name": "varlog"}]' >deployed-1.json
+      cat deployed-1.json | jq '.spec.template.spec.volumes += [{"name": "varlog", "persistentVolumeClaim": { "claimName": "logs-pco-log4j"} }]' >deployed-2.json
+      cat deployed-2.json | jq '.spec.template.spec.containers[0].env[1].value="true"' >deployed-3.json
+
+#oc -n ${NAMESPACE} get deployment ${DEPLOYMENT_NAME} -o json | jq '.spec.template.spec.containers[0].env[1].value="true"' | oc -n ${NAMESPACE} replace --force -f -
    else
-      echo "Re-applying the deployment..."
-      oc -n ${NAMESPACE} get deployment ${DEPLOYMENT_NAME} -o json | jq '.spec.selector.matchLabels.'${MATCH_SELECTOR}'="true" | .metadata.labels.'${MATCH_SELECTOR}'="true" | .spec.template.metadata.labels.'${MATCH_SELECTOR}'="true"' | oc -n ${NAMESPACE} replace --force -f -
-      set -x
-      if oc rollout status deploy/${DEPLOYMENT_NAME} --watch=true --request-timeout="900s" --namespace ${NAMESPACE}; then
-        STATUS="pass"
-      else
-        STATUS="fail"
-      fi
-      set +x
-      if [ "$STATUS" == "fail" ]; then
-        echo "DEPLOYMENT FAILED"
-        exit 1
-      fi
+      echo "Updating Match Selectors and enabling metrics and setting log4j PVC..."
+      cat deployed.json  | jq '.spec.template.spec.containers[0].env[1].value="true" | .spec.selector.matchLabels.'${MATCH_SELECTOR}'="true" | .metadata.labels.'${MATCH_SELECTOR}'="true" | .spec.template.metadata.labels.'${MATCH_SELECTOR}'="true"' >deployed-0.json
+	  cat deployed-0.json | jq '.spec.template.spec.containers[0].volumeMounts += [{"mountPath": "/home/aceuser/ace-server/log4j/logs", "name": "varlog"}]' >deployed-1.json
+      cat deployed-1.json | jq '.spec.template.spec.volumes += [{"name": "varlog", "persistentVolumeClaim": { "claimName": "logs-pco-log4j"} }]' >deployed-2.json
+      cat deployed-2.json | jq '.spec.template.spec.containers[0].env[1].value="true"' >deployed-3.json
+
+#oc -n ${NAMESPACE} get deployment ${DEPLOYMENT_NAME} -o json | jq '.spec.template.spec.containers[0].env[1].value="true" | .spec.selector.matchLabels.'${MATCH_SELECTOR}'="true" | .metadata.labels.'${MATCH_SELECTOR}'="true" | .spec.template.metadata.labels.'${MATCH_SELECTOR}'="true"' | oc -n ${NAMESPACE} replace --force -f -
+	  
+   fi
+
+   echo "Re-applying the deployment - modified deploy json follows..."
+   
+   cat deployed-3.json
+   
+   oc -n ${NAMESPACE} replace --force -f deployed-3.json
+   
+   set -x
+   if oc rollout status deploy/${DEPLOYMENT_NAME} --watch=true --request-timeout="1200s" --namespace ${NAMESPACE}; then
+      STATUS="pass"
+   else
+     STATUS="fail"
+   fi
+   set +x
+   if [ "$STATUS" == "fail" ]; then
+     echo "DEPLOYMENT FAILED"
+     exit 1
    fi
 fi
